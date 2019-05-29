795603e5c28908b310b753aa9e5a32f538493e37
Luiz Fernando Surian Filho <lfsurianfilho@gmail.com>
2019-05-29 15:42:51 -0300

    Add files via upload

--- database/nist_800_53_en.sql
+++ database/nist_800_53_en.sql
@@ -404,7 +404,7 @@ a. Develops, documents, and disseminates to [Assignment: organization-defined pe
 2. Procedures to facilitate the implementation of the configuration management policy and associated configuration management controls; and 
 b. Reviews and updates the current: 
 1. Configuration management policy [Assignment: organization-defined frequency]; and 
-2. Configuration management procedures [Assignment: organization-defined frequency]. ",
+2. Configuration management procedures [Assignment: organization-defined frequency].",
 "This control addresses the establishment of policy and procedures for the effective implementation of selected security controls and control enhancements in the CM family. Policy and procedures reflect applicable federal laws, Executive Orders, directives, regulations, policies, standards, and guidance. Security program policies and procedures at the organization level may make the need for system-specific policies and procedures unnecessary. The policy can be included as part of the general information security policy for organizations or conversely, can be represented by multiple policies reflecting the complex nature of certain organizations. The procedures can be established for the security program in general and for particular information systems, if needed. The organizational risk management strategy is a key factor in establishing policy and procedures."
 ),
 (48, 4, 2, 1, 1, --CM-2
